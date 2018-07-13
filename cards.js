2819a701365efa00c618184dc2f358accfd23ec4
Erika Weil <erikaweil@erikas-mbp.lan>
2018-07-13 16:29:41 -0500

    finishing bones for players model

---
+++ cards.js
@@ -0,0 +1,32 @@
+/**contains the functionality for the cards in the exploding kittens
+ * game. There are 11 kinds of cards, each with unique functionality:
+ * 1. Attack card
+ *  -forces the next player to draw two cards
+ * 2. Beard card
+ * -does nothing
+ * 3. Watermelon card
+ * -does nothing
+ * 4. Taco card 
+ * -does nothing
+ * 5. Rainbow card 
+ * -does nothing
+ * 6. Defuse card
+ * -keeps you alive after drawing an "exploding" card
+ * 7. Exploding card
+ * -if the player who draws it has a defuse card, he/she can play 
+ * it and the exploding card goes back in the draw pile
+ * -if the player who draws it does NOT have a defuse card, he/she dies.
+ * 8. Favor card
+ * -allows the player who draws it to get a card from another player
+ * 9. Nope card
+ * -forces the player who played the last card to play again
+ * 10. Skip card
+ * -forces the next player to lose his/her turn
+ * 11. Shuffle card 
+ * -allows the player to shuffle the cards in the draw pile
+ * 
+ * The card class contains a value "type", and methods for each of these unique card types.
+ */
+
+ 
+
--- players.js
+++ players.js
@@ -16,11 +16,78 @@ are met, the player has the option to defuse the card or die. If not, the game p
 to respond again. 
 -shuffle the deck method
 their own hand */
+const inquirer = require('inquirer')
+/*types of questions: 
+-confirm (y/n)
+-input ('')
+-list (item1, item2, item3)
+-expand (autofills for different inputs, 'h' for 'help, show me all the options')
+*/
+// var questions = [
+//     {
+//       type: 'confirm',
+//       name: 'toBeDelivered',
+//       message: 'Is this for delivery?',
+//       default: false
+//     },
+//     {
+//       type: 'input',
+//       name: 'phone',
+//       message: "What's your phone number?",
+//       validate: function(value) {
+//         var pass = value.match(
+//           /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+//         );
+//         if (pass) {
+//           return true;
+//         }
+  
+//         return 'Please enter a valid phone number';
+//       }
+//     },
+//     {
+//       type: 'list',
+//       name: 'size',
+//       message: 'What size do you need?',
+//       choices: ['Large', 'Medium', 'Small'],
+//       filter: function(val) {
+//         return val.toLowerCase();
+//       }
+//     },
+//     {
+//       type: 'expand',
+//       name: 'toppings',
+//       message: 'What about the toppings?',
+//       choices: [
+//         {
+//           key: 'p',
+//           name: 'Pepperoni and cheese',
+//           value: 'PepperoniCheese'
+//         },
+//         {
+//           key: 'a',
+//           name: 'All dressed',
+//           value: 'alldressed'
+//         },
+//         {
+//           key: 'w',
+//           name: 'Hawaiian',
+//           value: 'hawaiian'
+//         }
+//       ]
+//     }
+// ]
 
+// inquirer.prompt(questions).then(answers => {
+//     console.log('\nOrder receipt:');
+//     console.log('raw answers ', answers)
+//     console.log(JSON.stringify(answers, null, '  '));
+//   });
 class Player {
-    constructor(cards, playerId) {
+    constructor(cards, playerId, playerNickname) {
         this.hand = cards
         this.playerId = playerId
+        this.playerNickname = playerNickname || null
     }
     discard(card, discardPile) {
         [card].concat(discardPile)
@@ -32,9 +99,9 @@ class Player {
     respond(attack) {
     let choice = null;
     //check hand
-    if (this.hand.contains(skip || nope)) {
-        choice = 
-    }    
+    // if (this.hand.contains(skip || nope)) {
+    //     choice = 
+    // }    
         //skip()-- do I have a skip card? if so, prompt user to respond y/n to play it
         //draw two cards
         //give card to another player
@@ -43,26 +110,67 @@ class Player {
     count(deck) {
         return this.deck.length
     }
-    steal(player, option, n) {
-
+    steal(player, option) {
+        if (option == "view") {
+          var cardOptions = [{
+                  type: 'list',
+                  name: 'cards',
+                  message: 'Which card would you like to steal?',
+                  choices: [player.hand],
+                  filter: function(val) {
+                    return val.toLowerCase();
+                  }
+            }]
+            inquirer.prompt(cardOptions).then(answer => {
+              this.hand = this.hand.concat([cardOptions])
+              player.hand = player.hand.filter(card => card !== cardOptions)
+            })
+        } else { //the code below is NOT correct-- simply copy/paste from above. 
+          var cardOptions = [{
+            type: 'list',
+            name: 'cards',
+            message: 'Which card would you like to give?',
+            choices: [player.hand],
+            filter: function(val) {
+              return val.toLowerCase();
+            }
+      }]
+        inquirer.prompt(cardOptions).then(answer => {
+          this.hand = this.hand.concat([cardOptions])
+          player.hand = player.hand.filter(card => card !== cardOptions)
+        })
+      }
     }
     skip(turnQueue) {
-
+      let skipped = turnQueue[0]
+      turnQueue = turnQueue.slice(1).concat[skipped]
     }
     seeTheFuture(deck) {
-
+      return [deck[0], deck[1], deck[2]]
     }
     die(turnQueue) {
-
+      console.log(`EXPLOSION!! Player ${this.playerId} is dead ☠️`)
+      return turnQueue.filter(player => player.playerId !== this.playerId)
     }
-    defuse(drawPile) {
-
+    defuse(drawPile, explodingCard) {
+      let rdmIdx = Math.random() * drawPile.length
+      drawPile = drawPile.slice(0, rdmIdx).concat([explodingCard]).concat(drawPile.slice(rdmIdx))
     }
-    nope(card, player, turnQueue) {
+    nope(card) {
+      console.log(`Your ${card.title} has been noped. Try again.`)
 
     }
     shuffle(deck) {
-
+      let i = 0,
+          j = 0,
+          temp = null
+      for (i = deck.length - 1; i >= 0; i--) {
+        j = Math.floor(Math.random() * i + 1)
+        temp = deck[i]
+        deck[i] = deck[j]
+        deck[j] = temp
+      }
+      return deck
     }
 
 }
